297b67c8cf724daf796b50c0e858934e908d8d7d
ann-mj <87982338+ann-mj@users.noreply.github.com>
2022-01-29 14:57:31 +0900

    boj 문제 분류 추가

---
+++ src/boj/q11725/Main.java
@@ -0,0 +1,90 @@
+package boj.q11725;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    // 인접리스트로 트리를 표현해보자.
+    static ArrayList<Integer>[] adj;
+    static int[] parent;
+
+    static void input() {
+        N = scan.nextInt();
+        adj = new ArrayList[N+1];
+        parent = new int[N+1];
+        for(int i=1; i<=N; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        for(int i=1; i<N; i++) {
+            int x = scan.nextInt(), y = scan.nextInt();
+            adj[x].add(y); // x -> y 로 가는 것
+            adj[y].add(x);
+        }
+    }
+
+    static void dfs(int x, int par) {
+        for(int y : adj[x]) {
+            if(y == par) continue;
+            parent[y] = x;
+            dfs(y, x);
+        }
+    }
+
+    static void pro() {
+        // 1번 노드 부터 시작
+        dfs(1, -1);
+
+        for(int i=2; i<=N; i++) {
+            sb.append(parent[i]).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        Integer nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
---
+++ src/boj/q8958/Main.java
@@ -0,0 +1,37 @@
+package boj.q8958;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int testCount = scanner.nextInt();
+        ArrayList<String> inputs = new ArrayList<>();
+        for (int i = 0; i < testCount; i++) {
+            inputs.add(scanner.next());
+        }
+
+        for (String input : inputs) {
+            process(input);
+        }
+    }
+
+    public static void process(String input) {
+        char[] str = input.toCharArray();
+        int sum = 0;
+        int score = 0;
+        for (int i = 0; i < str.length; i++) {
+            if(str[i] == 'X') {
+                score = 0;
+                continue;
+            }
+
+            if(str[i] == 'O') {
+                score++;
+            }
+            sum += score;
+        }
+        System.out.println(sum);
+    }
+}
